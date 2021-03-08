64c25cd886a946e87ae5c9c86f7828688f0d7269
mazumdarabhinav <mazumdarabhinav@gmail.com>
2021-03-08 22:44:37 +0530

    Flask APP working

---
+++ docker/bank_note_authenticatio_classification/README.md
@@ -0,0 +1,4 @@
+# THINGS WHICH ARE COVERED
+ *1*.  Building and saving a simple binary classifier
+
+ *2.* Building an Flask App for this classifier
---
+++ docker/bank_note_authenticatio_classification/flask_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import pickle
+
+app = Flask(__name__)  # From which point to start the flask application
+# Load the pickle file
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
+
+
+@app.route("/")
+def welcome():
+    return "Welcome ALL"
+
+
+@app.route("/predict")
+def predict_note_autentication():
+    variance = request.args.get("variance")
+    skweness = request.args.get("skweness")
+    curtosis = request.args.get("curtosis")
+    entropy = request.args.get("entropy")
+
+    prediction = classifier.predict([[variance, skweness, curtosis, entropy]])
+    return "The predicted value is :" + str(prediction)
+
+
+@app.route("/predict_file", methods=["POST"])
+def predict_note_autentication():
+    variance = request.args.get("variance")
+    skweness = request.args.get("skweness")
+    curtosis = request.args.get("curtosis")
+    entropy = request.args.get("entropy")
+
+    prediction = classifier.predict([[variance, skweness, curtosis, entropy]])
+    return "The predicted value is :" + str(prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
